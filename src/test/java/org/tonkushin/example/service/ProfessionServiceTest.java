84bcc4f9634053f69a01ece2443b989cfe9d993f
itprom <aatonkushin@gmail.com>
2021-07-21 20:40:20 +0500

    Закончил Unit-тесты для ProfessionService

--- src/test/java/org/tonkushin/example/service/ProfessionServiceTest.java
+++ src/test/java/org/tonkushin/example/service/ProfessionServiceTest.java
@@ -11,6 +11,8 @@ import org.tonkushin.example.repository.ProfessionRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @TestPropertySource("/test.properties")
@@ -41,6 +43,57 @@ public class ProfessionServiceTest {
     }
 
     @Test()
+    public void saveNewProfessionWithLargeNameShouldThrowException() {
+        Profession profession = new Profession();
+        profession.setName(generateString(257));
+
+        ProfessionRepository repository = Mockito.mock(ProfessionRepository.class);
+        Mockito.when(repository.save(profession)).thenReturn(profession);
+
+        ProfessionService service = new ProfessionServiceImpl(repository);
+        Assertions.assertThatThrownBy(() -> service.save(profession))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("Поле 'Наименование' не должно превышать 255 символов");
+    }
+
+    @Test()
+    public void saveNewProfessionWithLargeNotesShouldThrowException() {
+        Profession profession = new Profession();
+        profession.setName("Профессия");
+        profession.setNotes(generateString(256));
+
+        ProfessionRepository repository = Mockito.mock(ProfessionRepository.class);
+        Mockito.when(repository.save(profession)).thenReturn(profession);
+
+        ProfessionService service = new ProfessionServiceImpl(repository);
+        Assertions.assertThatThrownBy(() -> service.save(profession))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("Поле 'Примечание' не должно превышать 255 символов");
+    }
+
+    public void saveNewProfessionWithExistingNameShouldThrowException() {
+        String name = "Профессия";
+
+        Profession profession = new Profession();
+        profession.setName(name);
+
+        Profession existingProfession = new Profession();
+        profession.setId(1);
+        profession.setName(name);
+
+        Optional<Profession> existingOpt = Optional.of(existingProfession);
+
+
+        ProfessionRepository repository = Mockito.mock(ProfessionRepository.class);
+        Mockito.when(repository.findByName(name)).thenReturn(existingOpt);
+
+        ProfessionService service = new ProfessionServiceImpl(repository);
+        Assertions.assertThatThrownBy(() -> service.save(profession))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining(String.format("Профессия с именем '%s' уже существует", profession.getName()));
+    }
+
+    @Test()
     public void findAllProfessions() {
         List<Profession> professions = new ArrayList<>(3);
         for (int i = 0; i < 3; i++) {
@@ -54,5 +107,51 @@ public class ProfessionServiceTest {
 
         ProfessionService service = new ProfessionServiceImpl(repository);
         Assertions.assertThat(service.findAll().size()).isEqualTo(3);
+    }
+
+    @Test
+    public void findById() {
+        Profession profession = new Profession();
+        profession.setId(1);
+        profession.setName("Профессия");
+
+        Optional<Profession> opt = Optional.of(profession);
+
+        ProfessionRepository repository = Mockito.mock(ProfessionRepository.class);
+        Mockito.when(repository.findById(1L)).thenReturn(opt);
+
+        ProfessionService service = new ProfessionServiceImpl(repository);
+        Assertions.assertThat(service.findOne(1L)).isEqualTo(profession);
+    }
+
+    @Test
+    public void findByNotExistingIdShouldThrowException() {
+        Profession profession = new Profession();
+        profession.setId(1);
+        profession.setName("Профессия");
+
+        Optional<Profession> opt = Optional.of(profession);
+
+        ProfessionRepository repository = Mockito.mock(ProfessionRepository.class);
+        Mockito.when(repository.findById(1L)).thenReturn(opt);
+
+        ProfessionService service = new ProfessionServiceImpl(repository);
+        Assertions.assertThatThrownBy(() -> service.findOne(2L))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    private String generateString(int targetStringLength) {
+        int leftLimit = 97;     // 'a'
+        int rightLimit = 122;   // 'z'
+
+        Random random = new Random();
+
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        return buffer.toString();
     }
 }
